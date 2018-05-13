bb27cfd04b658376bddd0d777a3e776da73e7c3d
ItIsMagic <morabi.andrea@gmail.com>
2018-05-13 15:27:32 +0200

    Change to homepage

--- homepage.php
+++ homepage.php
@@ -11,19 +11,6 @@
 	</div>
 	<div class="articlePartTitle backgroundArticle">Blockers  News</div>
 	<div id="article">
-		<div class="article">
-			<div class="backgroundArticle">
-				<div class="articleTitle">Join the Blockers  !</div>
-				<div class="articleBody">Scrip::Blocks has taken off, download the app now to join the Blockers in this Scripting journey!
-				We are really happy that so many people contribute to this project and we are eager to see what this comunity will build with our app, for any feedback, questions or small talk with our team go on our twitter accounts !</div>
-			</div>
-		</div>
-		<div class="article">
-			<div class="backgroundArticle">
-				<div class="articleTitle">The Blocks Rocket is soon taking off!</div>
-				<div class="articleBody">Prepare yourself for a revolution in the way you were scripting before !</div>
-			</div>
-		</div>
 	</div>
 </body>
 
